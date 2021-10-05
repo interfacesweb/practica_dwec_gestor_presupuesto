330365eb20d39714c4dfe41bf64fbb88c261b80c
Pedro Prieto <pedpral@gmail.com>
2021-10-05 13:18:48 +0200

    Probando varios workflows

---
+++ test/test1.js
@@ -0,0 +1,13 @@
+// Importar librería Chai para realizar aserciones
+import {assert} from "chai";
+
+// Importar código de la aplicación sobre la que se realizarán los tests
+import {actualizarPresupuesto, mostrarPresupuesto, CrearGasto} from '../js/gestionPresupuesto.js';
+
+// Inicialización de la variable global presupuesto
+describe("Testeo de prueba", function() {
+    it("TEst de prueba", function() {
+        assert.equal(true,true);
+    });
+});
+
